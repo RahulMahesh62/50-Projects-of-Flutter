cc70fb1bbf41a882c141fbcc13de618b11b7787a
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2021-01-23 09:02:28 +0530

    Added 2nd project

--- ID-Card/lib/main.dart
+++ ID-Card/lib/main.dart
@@ -20,61 +20,57 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[900],
-        elevation: 0.0
-      ),
+          title: Text('ID Card'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[900],
+          elevation: 0.0),
       backgroundColor: Colors.grey[700],
       body: Padding(
         padding: const EdgeInsets.all(35.0),
         child: Center(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 50.0),
-                CircleAvatar(
-                    backgroundImage: AssetImage('images/messi.jpg'),
-                    radius: 70.0,
-                  ),
-                  Divider(
-                    height: 50.0,
-                    color: Colors.grey[400],
-                  ),
-                  Text(
-                    'Lionel Messi',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.yellow[600],
-                      letterSpacing: 2.0
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    'Centre-Forward, FC Barcelona',
-                    style: TextStyle(
-                      fontSize: 17.0,
-                      color: Colors.purple[900],
-                      letterSpacing: 1.5
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    '6x Golden-Ball winner',
-                    style: TextStyle(
-                      fontSize: 17.0,
-                      color: Colors.pink[400],
-                      letterSpacing: 1.5
-                    ),
-                  ),
-                  SizedBox(height: 30.0),
-                  Image.asset(
-                    'images/barca.png',
-                    height: 170.0,
-                    width: 170.0,
-                  ),
-              ],
-            ),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 50.0),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/messi.jpg'),
+                radius: 70.0,
+              ),
+              Divider(
+                height: 50.0,
+                color: Colors.grey[400],
+              ),
+              Text(
+                'Lionel Messi',
+                style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.yellow[600],
+                    letterSpacing: 2.0),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Centre-Forward, FC Barcelona',
+                style: TextStyle(
+                    fontSize: 17.0,
+                    color: Colors.purple[900],
+                    letterSpacing: 1.5),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                '6x Golden-Ball winner',
+                style: TextStyle(
+                    fontSize: 17.0,
+                    color: Colors.pink[400],
+                    letterSpacing: 1.5),
+              ),
+              SizedBox(height: 30.0),
+              Image.asset(
+                'images/barca.png',
+                height: 170.0,
+                width: 170.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
